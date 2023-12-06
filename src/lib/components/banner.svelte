309c6d40698be8a212d35b6b940a4575edacf050
Mnichus3142 <mstrojek8@gmail.com>
2023-12-06 11:43:17 +0100

    start

---
+++ src/lib/components/banner.svelte
@@ -0,0 +1,11 @@
+<script>
+    
+</script>
+
+<header>
+    AAA
+</header>
+
+<style>
+
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    import Banner from '$lib/components/banner.svelte'
+</script>
+
+<body>
+    <Banner></Banner>
+</body>
+
+<style>
+    
+</style>
