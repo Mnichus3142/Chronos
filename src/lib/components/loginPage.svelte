d913c2b5536351ad3d812e12a0e735babc3ece0d
Mnichus3142 <mstrojek8@gmail.com>
2024-01-17 19:41:19 +0100

    login

---
+++ src/lib/components/loginPage.svelte
@@ -0,0 +1,42 @@
+<script>
+
+</script>
+
+<div>
+    <div class="formBorder">
+
+    </div>
+    <form class="form">
+        
+    </form>
+</div>
+
+<style>
+    .formBorder {
+        width: calc(60vw + 20px);
+        height: calc(60vh + 20px);
+        position: absolute;
+        margin-left: -10px;
+        margin-top: -10px;
+        background-color: blue;
+        z-index: 0;
+
+        background: linear-gradient(
+            to bottom,
+            var(--1),
+            var(--3)
+        );
+
+        border-radius: 20px;
+    }
+
+    .form {
+        position: relative;
+        width: 60vw;
+        height: 60vh;
+        background-color: var(--white);
+        z-index: 4;
+
+        border-radius: 10px;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,15 @@
 <script>
     import Banner from '$lib/components/banner.svelte'
+    import LoginPage from '$lib/components/loginPage.svelte'
 </script>
 
 <body>
     <Banner></Banner>
+    <div class="content">
+        <LoginPage>
+
+        </LoginPage>
+    </div>
 </body>
 
 <style>
@@ -16,5 +22,15 @@
     body
     {
         overflow-x: hidden;
+    }
+
+    .content {
+        background-color: var(--white);
+        width: 100vw;
+        height: calc(97.5vh - 60px);
+    
+        display: flex;
+        justify-content: center;
+        place-items: center;
     }
 </style>
